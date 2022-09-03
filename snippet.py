afd66b236c0b11a60443a0bc5d62914e35e8cbb7
Massimiliano <105291465+max45556@users.noreply.github.com>
2022-09-03 14:47:56 +0200

    Update snippet.py

--- snippet.py
+++ snippet.py
@@ -1,8 +1,27 @@
-def merge(*args, missing_val = None):
-#missing_val will be used when one of the smaller lists is shorter tham the others.
-#Get the maximum length within the smaller lists.
-  max_length = max([len(lst) for lst in args])
-  outList = []
-  for i in range(max_length):
-    result.append([args[k][i] if i < len(args[k]) else missing_val for k in range(len(args))])
-  return outList
+from my_lib import Object
+
+import os
+
+from my_lib import Object3
+
+from my_lib import Object2
+
+import sys
+
+from third_party import lib15, lib1, lib2, lib3, lib4, lib5, lib6, lib7, lib8, lib9, lib10, lib11, lib12, lib13, lib14
+
+import sys
+
+from __future__ import absolute_import
+
+from third_party import lib3
+
+def sort_by_indexes(lst, indexes, reverse=False):
+  return [val for (_, val) in sorted(zip(indexes, lst), key=lambda x: \
+  x[0], reverse=reverse)]
+
+a = ['eggs', 'bread', 'oranges', 'jam', 'apples', 'milk']
+b = [         3, 2, 6, 4, 1, 5]
+sort_by_indexes(a, b) # ['apples', 'bread', 'eggs', 'jam', 'milk', 'oranges']
+  sort_by_indexes(a, b, True)
+# ['oranges', 'milk', 'jam', 'eggs', 'bread', 'apples']
