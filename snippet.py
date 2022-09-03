90498dd07cce39b091c9cb82e9d316a5bcaf177b
Massimiliano <105291465+max45556@users.noreply.github.com>
2022-09-03 16:27:46 +0200

    Update snippet.py

--- snippet.py
+++ snippet.py
@@ -17,12 +17,12 @@ from __future__ import absolute_import
 from third_party import lib3
 
 def sort_by_indexes(lst, indexes, reverse=False):
-  return [val for (_, val) in sorted(zip(indexes, lst), key=lambda x: \
+  return [val for (_, val) in sorted(zip(indexes, lst), key=lambda x: 
   x[0], reverse=
   reverse)]
 
 a = ['eggs', 'bread', 'oranges', 'jam', 'apples', 'milk']
-b = [3, 2, 
+b = [3, 2,
      6, 4,
      1, 5   ]
 sort_by_indexes(a,     b) # ['apples', 'bread', 'eggs', 'jam', 'milk', 'oranges']
