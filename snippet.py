e5af1f8542f2c9e3678e59c600702e6a506c58ca
Massimiliano <105291465+max45556@users.noreply.github.com>
2022-09-03 15:06:01 +0200

    snippet modified

--- snippet.py
+++ snippet.py
@@ -18,10 +18,13 @@ from third_party import lib3
 
 def sort_by_indexes(lst, indexes, reverse=False):
   return [val for (_, val) in sorted(zip(indexes, lst), key=lambda x: \
-  x[0], reverse=reverse)]
+  x[0], reverse=
+  reverse)]
 
 a = ['eggs', 'bread', 'oranges', 'jam', 'apples', 'milk']
-b = [         3, 2, 6, 4, 1, 5]
-sort_by_indexes(a, b) # ['apples', 'bread', 'eggs', 'jam', 'milk', 'oranges']
-  sort_by_indexes(a, b, True)
+b = [3, 2, \
+     6, 4,
+     1, 5   ]
+sort_by_indexes(a,     b) # ['apples', 'bread', 'eggs', 'jam', 'milk', 'oranges']
+sort_by_indexes(a, b  , True  )
 # ['oranges', 'milk', 'jam', 'eggs', 'bread', 'apples']
